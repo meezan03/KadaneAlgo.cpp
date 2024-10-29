caf378c3cdb25a97129e407024fb767c5dec17e1
meezan03 <meezanfaisal786@gmail.com>
2024-10-29 22:35:34 +0530

    Add files via upload

---
+++ kadaneAlgo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+int maxsubArray( vector<int>& nums){
+    int currSum = 0,  maxSum = INT_MIN;
+    for(int val : nums){
+        currSum += val;
+        maxSum = max(currSum, maxSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
+
+int main(){
+    vector <int> nums = {3, -4, 5, 4, -1, 7, -8};
+
+    cout <<"max subarray sum ="<< maxsubArray(nums) <<endl;
+
+    return 0;
+}
